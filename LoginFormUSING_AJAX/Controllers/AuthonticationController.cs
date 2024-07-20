a469eb65ce3ffb014a220e39e7fb052cb1ec41fa
Apnacollege-demo <radhakadam500@gmail.com>
2024-07-20 16:42:58 +0530

    Add project files.

---
+++ LoginFormUSING_AJAX/Controllers/AuthonticationController.cs
@@ -0,0 +1,26 @@
+﻿using LoginFormUSING_AJAX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginFormUSING_AJAX.Controllers
+{
+    public class AuthonticationController : Controller
+    {
+        // GET: Authontication
+        MaveRickEntities db=new MaveRickEntities(); 
+        public ActionResult IndexLogin()
+        {
+            return View();
+        }
+        public JsonResult SaveData(LoginTb tb)
+        {
+            db.LoginTbs.Add(tb);
+            db.SaveChanges();
+            return Json("Registration Successfull",JsonRequestBehavior.AllowGet);
+        }
+            
+    }
+}
